0e489329cc4952c2d0ff298df16f7ffd25c125aa
Brendan Evans <brendan@brendanevans.com.au>
2019-02-27 06:49:27 +1000

    update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,50 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import requests
+import json
+import datetime
+import dateutil.parser
+import scraperwiki
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+def process(applications):
+    url = 'https://www.casey.vic.gov.au'
+    for row in applications['rows']:
+        output = {}
+        output['council_reference'] = row['application_number']
+        output['address'] = row['field_coc_address'] + row['field_coc_suburb_ref']
+        output['description'] = row['field_coc_proposal']
+        output['info_url'] = url + row['field_coc_pdf'].strip('\n')
+        output['comment_url'] = 'mailto:caseycc@casey.vic.gov.au'
+        dateScraped = datetime.date.today().isoformat()
+        output['date_scraped'] = dateScraped
+        output['date_received'] = ''
+        output['on_notice_from'] = ''
+        date = dateutil.parser.parse(row['field_coc_date'], dayfirst=True)
+        output['on_notice_to'] = date.strftime('%Y-%m-%d')
+        try:
+            alreadyExists = scraperwiki.sql.select('* FROM data WHERE council_reference=?', [da['council_reference']])
+        except:
+            alreadyExists = False
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+        if alreadyExists:
+            print('Skipping: {}'.format(output['council_reference']))
+        else:
+            print('Saving: {}'.format(output['council_reference']))
+            scraperwiki.sql.save(unique_keys=['council_reference'], data=output, table_name='data')
+
+url = 'https://www.casey.vic.gov.au/api/planning-applications'
+
+querystring = {'_format':'json', 'page':'0'}
+payload = ''
+response = requests.request('GET', url, data=payload, params=querystring)
+
+applications = response.json()
+process(applications)
+
+pages = applications['pager']['total_pages']
+if pages > 1:
+    i = 1
+    while i < pages:
+        querystring = {'_format':'json', 'page':i}
+        response = requests.request('GET', url, data=payload, params=querystring)
+        applications = response.json()
+        process(applications)
+        i += 1
